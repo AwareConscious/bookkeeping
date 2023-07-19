8b4e58dbfde7496a3880a291cbd3ce407a87f421
Yevgeniy Yuzhaninov <jahdao@gmail.com>
2023-07-20 01:16:43 +0600

    Switch to text dialog interface

--- src/main/java/org/bookkeeping/Main.java
+++ src/main/java/org/bookkeeping/Main.java
@@ -11,54 +11,62 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Employee employee = new Employee();
+        String command = "help";
 
-        if (args[0].equals("help")) {
+        while (!command.equals("exit")) {
+
             System.out.println("" +
-                    "bookkeeping [OPTIONS] COMMAND\n\n" +
-                    "Commands:\n" +
+                    "Enter one of commands:\n" +
                     "add    :adds new employee to employee registry\n" +
                     "delete :deletes employee from employee registry\n" +
                     "edit   :edits existed employee\n" +
                     "list   :lists all employees in registry\n" +
-                    "help   :shows this help message");
-        }
+                    "help   :shows this message");
+            command = scanner.nextLine();
 
-        if (args[0].equals("add")) {
+            if (command.equals("add")) {
+                boolean addMore = true;
+                while (addMore) {
+                    employee.setId(1);
+                    System.out.println("Enter employee first name: ");
+                    employee.setFirstName(scanner.nextLine());
+                    System.out.println("Enter employee second name: ");
+                    employee.setSecondName(scanner.nextLine());
+                    System.out.println("Enter employee last name: ");
+                    employee.setLastName(scanner.nextLine());
+                    System.out.println("Enter employee salary: ");
+                    employee.setSalary(new BigDecimal(scanner.nextLine()));
+                    System.out.println("Enter employee permanent premium: ");
+                    employee.setPermanentPremium(new BigDecimal(scanner.nextLine()));
+                    System.out.println("Is employee has residence of Kazakhstan (y/n): ");
+                    employee.setResidentOfKazakhstan(scanner.nextLine().equals("y"));
+                    System.out.println("Enter employee employment date in format dd.mm.yyyy: ");
+                    String date = scanner.nextLine();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+                    try {
+                        //Parsing the String
+                        Date date2 = dateFormat.parse(date);
+                        employee.setEmploymentDate(date2);
+                    } catch (ParseException e) {
+                        System.out.println("ERROR: Date parsing failed ");
+                        e.printStackTrace();
+                    }
+                    System.out.println("" +
+                            "Employee: " + employee.getFirstName() + " " + employee.getSecondName() + " " +
+                            employee.getLastName() + " successfully added to registry");
+                    System.out.println("Would you like to add one more employee ? (y/n): ");
+                    addMore = scanner.nextLine().equals("y");
 
-            employee.setId(1);
-            System.out.println("Enter employee first name: ");
-            employee.setFirstName(scanner.nextLine());
-            System.out.println("Enter employee second name: ");
-            employee.setSecondName(scanner.nextLine());
-            System.out.println("Enter employee last name: ");
-            employee.setLastName(scanner.nextLine());
-            System.out.println("Enter employee salary: ");
-            employee.setSalary(new BigDecimal(scanner.nextLine()));
-            System.out.println("Enter employee permanent premium: ");
-            employee.setPermanentPremium(new BigDecimal(scanner.nextLine()));
-            System.out.println("Is employee has residence of Kazakhstan (y/n): ");
-            employee.setResidentOfKazakhstan(scanner.nextLine().equals("y"));
-            System.out.println("Enter employee employment date in format dd.mm.yyyy: ");
-            String date = scanner.nextLine();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-            try {
-                //Parsing the String
-                Date date2 = dateFormat.parse(date);
-                employee.setEmploymentDate(date2);
-            } catch (ParseException e) {
-                System.out.println("ERROR: Date parsing failed ");
-                e.printStackTrace();
+                }
             }
-            System.out.println("" +
-                    "Employee: " + employee.getFirstName() + " " + employee.getSecondName() + " " +
-                    employee.getLastName() + " successfully added to registry");
-        }
 
-        if (args[0].equals("list")) {
-            System.out.println(employee.getFirstName() + " " + employee.getSecondName() + " " +
-                    employee.getLastName() + " " + employee.getId() + " " + employee.getSalary() +
-                    " " + employee.getPermanentPremium() + employee.isResidentOfKazakhstan() + " " +
-                    employee.getEmploymentDate());
+            if (command.equals("list")) {
+                System.out.println(employee.getFirstName() + " " + employee.getSecondName() + " " +
+                        employee.getLastName() + " " + employee.getId() + " " + employee.getSalary() +
+                        " " + employee.getPermanentPremium() + " " + employee.isResidentOfKazakhstan() + " " +
+                        employee.getEmploymentDate());
+
+            }
         }
     }
 }
